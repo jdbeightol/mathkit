27a2f92e815a5560328f74a20c03e35150d99e15
Jeremy Beightol <jdbeightol@gmail.com>
2014-07-30 09:45:25 -0400

    Created new data encapsulation class files.

---
+++ src/mathtoolkit/DataSet.java
@@ -0,0 +1,44 @@
+package mathtoolkit;
+
+public class DataSet
+{
+    public String[] maxVariables,
+                    maxSlackVars,
+                    minVariables,
+                    minSlackVars;
+    
+    Rational[][]    data;
+    
+    public DataSet()
+    {
+        data = null;
+        maxVariables = null;
+        minVariables = null;
+        maxSlackVars = null;
+        minSlackVars = null;
+    }
+    
+    public DataSet(Rational[][] in)
+    {
+        this();
+        data = in;
+    }
+    
+    public DataSet(Rational[][] in, String[] maxVars, String[] maxSlacks, String[] minVars, String[] minSlacks)
+    {
+        data = in;
+        maxVariables = maxVars;
+        minVariables = maxSlacks;
+        maxSlackVars = minVars;
+        minSlackVars = minSlacks;
+    }
+    
+    public boolean isDataNull()
+    {    return data == null;    }
+    
+    public boolean isMaxNull()
+    {    return maxVariables == null || maxSlackVars == null;    }
+    
+    public boolean isMinNull()
+    {    return minVariables == null || minSlackVars == null;    }
+}
---
+++ src/mathtoolkit/Point.java
@@ -0,0 +1,12 @@
+package mathtoolkit;
+
+public class Point
+{
+    int i, j;
+
+    public Point()
+    {    i = j = 0;    }
+
+    public Point(int i, int j)
+    {   this.i = i; this.j = j;   }
+}
