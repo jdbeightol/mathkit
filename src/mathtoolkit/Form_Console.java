4e3e24c082064ab58e2f6798243d0f7dd85c89d4
Jeremy Beightol <jdbeightol@gmail.com>
2014-07-31 09:54:57 -0400

    Exploring Git branches.  Also working some experimental options such as popping out a JInternalFrame and Frame docking.

--- src/mathtoolkit/Form_Console.java
+++ src/mathtoolkit/Form_Console.java
@@ -1,9 +1,14 @@
 package mathtoolkit;
 
+import java.awt.Container;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -11,6 +16,8 @@ import javax.swing.text.DefaultCaret;
 
 public class Form_Console extends javax.swing.JInternalFrame
 {
+    private JFrame popoutWindow = null;
+    
     private class ConsoleOutputStream extends OutputStream 
     {
         private final JTextArea console;
@@ -20,7 +27,7 @@ public class Form_Console extends javax.swing.JInternalFrame
         
         @Override
         public void write(int in) throws IOException
-        {    console.append(String.valueOf((char)in));    }  
+        {    console.append(String.valueOf((char)in));    }
     }
     
     public Form_Console()
@@ -41,14 +48,52 @@ public class Form_Console extends javax.swing.JInternalFrame
             public void changedUpdate(DocumentEvent e){}
         });
         
-        
         ((DefaultCaret)jTextArea1.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);        
         
         PrintStream st = new PrintStream(new ConsoleOutputStream(jTextArea1));
         
         System.setOut(st);
     }
+    
+    public void popout()
+    {
+        if(popoutWindow == null)
+            popoutWindow = new JFrame();
+        
+        popoutWindow.setSize(getSize());
+        popoutWindow.setTitle(title);
+        
+        popoutWindow.addWindowListener(new WindowAdapter() 
+        {
+            @Override
+            public void windowClosing(WindowEvent e) 
+            {
+                popin();
+            }
+        });
+        
+        popoutWindow.add(this.getContentPane());
+        popoutWindow.setJMenuBar(jMenuBar1);
+        
+        setVisible(false);
+        popoutWindow.setVisible(true);
+        jMenuItem2.setVisible(false);
+    }
+    
+    private void popin()
+    {
+        setContentPane(popoutWindow.getContentPane());
+        this.setJMenuBar(jMenuBar1);
+        jMenuItem2.setVisible(true);
+        setVisible(true);
+    }
 
+    public void dock(Container c)
+    {
+        c.add(this.getContentPane());
+        this.setVisible(false);
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents()
@@ -58,6 +103,7 @@ public class Form_Console extends javax.swing.JInternalFrame
         jTextArea1 = new javax.swing.JTextArea();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
 
         setIconifiable(true);
@@ -72,6 +118,16 @@ public class Form_Console extends javax.swing.JInternalFrame
         jScrollPane1.setViewportView(jTextArea1);
 
         jMenu1.setText("Text");
+
+        jMenuItem2.setText("Pop Out");
+        jMenuItem2.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                jMenuItem2ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem2);
 
         jMenuItem1.setText("Clear");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener()
@@ -106,10 +162,16 @@ public class Form_Console extends javax.swing.JInternalFrame
         jTextArea1.setText("");
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
+    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jMenuItem2ActionPerformed
+    {//GEN-HEADEREND:event_jMenuItem2ActionPerformed
+        popout();
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
