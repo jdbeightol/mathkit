9b4cea29695226d1ce0d7094a557a7ebe7567d00
Jeremy Beightol <jdbeightol@gmail.com>
2014-08-06 07:15:44 -0400

    Version 0.99.3

--- src/mathtoolkit/MathKit.java
+++ src/mathtoolkit/MathKit.java
@@ -2,7 +2,7 @@ package mathtoolkit;
 
 public class MathKit
 {
-    public static final String VERSION = "0.99.2";
+    public static final String VERSION = "0.99.3";
     
     private static boolean debug = false;
     
